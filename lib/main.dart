e75cbf8eba3bbf0a8b568c9d0b1769b6ee54308f
Trần Anh Quân <trananhquan2206>
2023-10-12 23:16:50 +0700

    Sửa Appbar, Avatar và thêm thanh bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Demo Material App'),
+  runApp(MaterialApp(
+    home: SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Profile"),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                // Xử lý khi nhấn
+              },
+            )
+          ],
+          backgroundColor: const Color.fromRGBO(254, 161, 124, 1),
+          leading: IconButton(
+            onPressed: () {
+              // Xử lý khi nhấn
+            },
+            icon: const Icon(Icons.menu),
           ),
-          body: const Center(child: Text("Material App"))),
-      darkTheme: ThemeData(primaryColor: Colors.grey),
-      color: Colors.amberAccent,
-      supportedLocales: {const Locale('en', ' ')},
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        ),
+        body: Column(children: [
+          Row(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage("assets/img/avatar.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+
+              )
+            ],
+          )
+        ]),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          fixedColor:const  Color.fromRGBO(254, 161, 124, 1),
+          items:const  [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: "Notifications",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),
+          ],
+        ),
       ),
-    );
-  }
+    ),
+    debugShowCheckedModeBanner: false,
+  ));
 }
