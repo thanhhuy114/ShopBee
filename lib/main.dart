fed0e001d78ccc0a184de90f5e3828ca8015077e
Thanh-Huy <huycherry14@gmail.com>
2023-10-17 23:40:16 +0700

    Revert "menu"
    This reverts commit 8f94f9879683550183a62358c48468466377171b.

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Menu'),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Menu',
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text('Home'),
-                onTap: () {
-                  // Xử lý khi người dùng chọn 'Home'
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.account_circle),
-                title: const Text('Account'),
-                onTap: () {
-                  // Xử lý khi người dùng chọn 'Account'
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.help),
-                title: const Text('Help Centre'),
-                onTap: () {
-                  // Xử lý khi người dùng chọn 'Help Centre'
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.exit_to_app),
-                title: const Text('Log Out'),
-                onTap: () {
-                  // Xử lý khi người dùng chọn 'Log Out'
-                },
-              ),
-            ],
+          appBar: AppBar(
+            title: const Text('Demo Material App'),
           ),
-        ),
+          body: const Center(child: Text("Material App"))),
+      darkTheme: ThemeData(primaryColor: Colors.grey),
+      color: Colors.amberAccent,
+      supportedLocales: {const Locale('en', ' ')},
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
     );
   }
