b5cf0f9026ce7f5c0efbbf2c964942280dfd2a69
Thanh-Huy <huycherry14@gmail.com>
2023-10-18 10:26:40 +0700

    new thong ke

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'info/statitics.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,89 +16,86 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-
           //Menu
           drawer: Drawer(
-            child:ListView(
-              children: <Widget>[
-                const DrawerHeader(
+              child: ListView(
+            children: <Widget>[
+              const DrawerHeader(
                   decoration: BoxDecoration(
                     color: Color.fromRGBO(254, 161, 124, 1.0),
                   ),
                   child: Padding(
-                    padding: EdgeInsets.only(left: 15.0,top: 15.0),
-                    child:Text('Shop Bee',style: TextStyle(fontSize: 25.0),),
-                  )
-                ),
-
-                ListTile(
-                  title: const Text('Hướng dẫn'),
-                  leading: const Icon(Icons.book),
-                  onTap:(){},
-                ),
-                ListTile(
-                  title: const Text('Hướng dẫn'),
-                  leading: const Icon(Icons.book),
-                  onTap:(){},
-                ),
-                ListTile(
-                  title: const Text('Hướng dẫn'),
-                  leading:const  Icon(Icons.book),
-                  onTap:(){},
-                ),
-                ListTile(
-                  title:const Text('Đăng Xuất'),
-                  leading: const Icon(Icons.logout),
-                  onTap: () {
-                  },
-                )
-              ],
-            )
-          ),
+                    padding: EdgeInsets.only(left: 15.0, top: 15.0),
+                    child: Text(
+                      'Shop Bee',
+                      style: TextStyle(fontSize: 25.0),
+                    ),
+                  )),
+              ListTile(
+                title: const Text('Hướng dẫn'),
+                leading: const Icon(Icons.book),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text('Hướng dẫn'),
+                leading: const Icon(Icons.book),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text('Hướng dẫn'),
+                leading: const Icon(Icons.book),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text('Đăng Xuất'),
+                leading: const Icon(Icons.logout),
+                onTap: () {},
+              )
+            ],
+          )),
 
           //App bar
           appBar: AppBar(
-            backgroundColor:const  Color.fromRGBO(254,161,124,1),
+            backgroundColor: const Color.fromRGBO(254, 161, 124, 1),
             centerTitle: true,
-
-            title:const Text('Thông Tin Cá Nhân',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
-            
+            title: const Text(
+              'Thông Tin Cá Nhân',
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+            ),
             actions: const [
               Padding(
-                padding:EdgeInsets.only(right: 15.0),
-                child:Icon(Icons.settings)
-              )
+                  padding: EdgeInsets.only(right: 15.0),
+                  child: Icon(Icons.settings))
             ],
           ),
-
 
           bottomNavigationBar: BottomNavigationBar(
             currentIndex: 3,
-            fixedColor:const  Color.fromRGBO(254,161,124,1),
+            fixedColor: const Color.fromRGBO(254, 161, 124, 1),
             items: const [
               BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined,color: Colors.black,),
-                label: 'Trang Chủ'
-              ),
+                  icon: Icon(
+                    Icons.home_outlined,
+                    color: Colors.black,
+                  ),
+                  label: 'Trang Chủ'),
               BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart_outlined,color: Colors.black),
-                label: 'Cửa Hàng'
-              ),
+                  icon: Icon(Icons.shopping_cart_outlined, color: Colors.black),
+                  label: 'Cửa Hàng'),
               BottomNavigationBarItem(
-                icon: Icon(Icons.notifications_none_sharp,color: Colors.black),
-                label: 'Thông Báo'
-              ),
+                  icon:
+                      Icon(Icons.notifications_none_sharp, color: Colors.black),
+                  label: 'Thông Báo'),
               BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline_outlined,),
-                label: 'Cá Nhân'
-              )
+                  icon: Icon(
+                    Icons.person_outline_outlined,
+                  ),
+                  label: 'Cá Nhân')
             ],
           ),
-          
 
           body: Column(
-            children:  [
-
+            children: [
               //Sử lý hình ảnh và thông tin
               Padding(
                 padding: const EdgeInsets.all(5.0),
@@ -104,63 +103,117 @@ class MyApp extends StatelessWidget {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(25.0),
                     border: Border.all(
-                      color: const  Color.fromRGBO(254, 161, 124, 1.0),
-                      width: 3.0
-                    ),
+                        color: const Color.fromRGBO(254, 161, 124, 1.0),
+                        width: 3.0),
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: Row(
-
                       children: [
-                        
                         ClipOval(
-                          child: Image.asset("assets/img/avatar.png",width: 110.0,height: 110.0,fit: BoxFit.cover,),
+                          child: Image.asset(
+                            "assets/img/avatar.png",
+                            width: 110.0,
+                            height: 110.0,
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                        
-                                
-                        const Padding(
-                          padding: EdgeInsets.only(left: 25.0), 
-                          child:Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Tên: Lê Văn C',style: TextStyle(fontSize: 17.0,color: Colors.black),),
-                              Text('Ngày Sinh: 01/01/2003',style: TextStyle(fontSize: 17.0,color: Colors.black),),
-                              Text('SĐT: 0333333333',style: TextStyle(fontSize: 17.0,color: Colors.black),),
-                              Text('Email: abc@gmail.com',style: TextStyle(fontSize: 17.0,color: Colors.black),),
-                              Text('Địa Chỉ: TP Hồ Chí Minh',style: TextStyle(fontSize: 17.0,color: Colors.black),),
-                            ],
-                          )
-                        )
+                        Padding(
+                            padding: const EdgeInsets.only(left: 25.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Tên: Lê Văn C',
+                                  style: TextStyle(
+                                      fontSize: 17.0, color: Colors.black),
+                                ),
+                                Text(
+                                  'Ngày Sinh: 01/01/2003',
+                                  style: TextStyle(
+                                      fontSize: 17.0, color: Colors.black),
+                                ),
+                                Text(
+                                  'SĐT: 0333333333',
+                                  style: TextStyle(
+                                      fontSize: 17.0, color: Colors.black),
+                                ),
+                                Text(
+                                  'Email: abc@gmail.com',
+                                  style: TextStyle(
+                                      fontSize: 17.0, color: Colors.black),
+                                ),
+                                Text(
+                                  'Địa Chỉ: TP Hồ Chí Minh',
+                                  style: TextStyle(
+                                      fontSize: 17.0, color: Colors.black),
+                                ),
+                              ],
+                            ))
                       ],
                     ),
                   ),
                 ),
               ),
 
-              //Sử lý phần thống kê
-              const Padding(
-                padding: EdgeInsets.all(5.0),
+              const StatisticsWidget(),
+
+              Container(
+                height: 60.0,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Column(
-                      children: [
-                        Icon(Icons.bedtime_off_rounded)
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        SizedBox(height: 4.0),
                       ],
                     ),
                     Column(
-              
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.attach_money, color: Colors.white),
+                        const SizedBox(height: 4.0),
+                        Text(
+                          'Số tiền: \$${100.toStringAsFixed(2)}',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ],
                     ),
                     Column(
-              
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.monetization_on, color: Colors.white),
+                        SizedBox(height: 4.0),
+                        Text(
+                          'Số xu: 100',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              )
+              ),
 
+              //Sử lý phần thống kê
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  children: [
+                    Column(
+                      children: const [Icon(Icons.bedtime_off_rounded)],
+                    ),
+                    Column(),
+                    Column(),
+                  ],
+                ),
+              )
             ],
           ),
-
         ),
       ),
       debugShowCheckedModeBanner: false,
